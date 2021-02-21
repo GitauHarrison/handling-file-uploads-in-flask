3fb49d3f3c46aba655cee2aed76252da4223fefd
harry <harrisongitau3@gmail.com>
2021-02-21 06:23:03 +0300

    submit uploaded file

--- app/routes.py
+++ app/routes.py
@@ -7,4 +7,9 @@ from app.forms import UploadForm
 @app.route('/index', methods=['GET', 'POST'])
 def index():
     form = UploadForm()
+    if request.method == 'POST':
+        uploaded_file = request.files['file']
+        if uploaded_file.filename != '':
+            uploaded_file.save(uploaded_file.filename)
+        return redirect(url_for('index'))
     return render_template('index.html', title='Home', form=form)
