7856a6da09c628a0d83eddd0783c942e4ff4519d
harry <harrisongitau3@gmail.com>
2021-12-10 17:32:59 +0300

    Dev: Rename admin table to user table

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from app import db
 
 
-class Admin(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
