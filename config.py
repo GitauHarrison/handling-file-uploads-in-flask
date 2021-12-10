c14cab64e499c9f39565267505b62049da19f30b
harry <harrisongitau3@gmail.com>
2021-12-10 18:36:45 +0300

    Fix: Update heroku postgres db url

--- config.py
+++ config.py
@@ -8,7 +8,8 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 
     # Database
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
+        'postgres://', 'postgresql://') or\
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
