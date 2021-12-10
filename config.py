34e01ad43fac7e2c3551b6d34ad2742ef7b285e3
harry <harrisongitau3@gmail.com>
2021-12-10 17:32:06 +0300

    Dev: Add path to uploaded file

--- config.py
+++ config.py
@@ -11,3 +11,6 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # File Upload
+    UPLOAD_PATH = os.environ.get('UPLOAD_PATH') or 'app/static/uploads'
