5cb9981f3272965dfd593be1a45e3873fe3dc580
harry <harrisongitau3@gmail.com>
2021-12-10 16:04:46 +0300

    Dev: Add user model

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 boostrap = Bootstrap(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes, errors
+from app import routes, errors, models
---
+++ app/models.py
@@ -0,0 +1,9 @@
+from app import db
+
+
+class Admin(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    about_me = db.Column(db.String(140))
+    avatar = db.Column(db.String(140))
